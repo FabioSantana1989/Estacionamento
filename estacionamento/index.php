204e685184a3f539172549b2587825fbeda62ac6
Fabio Santana <s.fabiosantana1989@gmail.com>
2016-03-07 22:03:23 -0300

    Inclusao de estilo na Pagina Inicial do Projeto.

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>Página Inicial do Projeto</title>
+	<link rel="stylesheet" href="bootstrap/min.css" />
 </head>
 <body>
 	<h1>Página Inicial do Projeto</h1>
