dc094c5c834318847bcc728068fb606c0db5c8f9
Fabio Santana <s.fabiosantana1989@gmail.com>
2016-03-07 21:54:41 -0300

    Estruturação da página Inicial do Projeto.

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -1,2 +1,10 @@
-<?php
-print "Pagina Inícial do Projeto!";
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Página Inicial do Projeto</title>
+</head>
+<body>
+	<h1>Página Inicial do Projeto</h1>
+</body>
+</html>
